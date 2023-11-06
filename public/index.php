bbdcca787e5ce7627d1a8b4453c50bae9a869059
DaCypher <73401065+DaCipher@users.noreply.github.com>
2023-11-07 00:41:24 +0100

    Bug fix

--- public/index.php
+++ public/index.php
@@ -2,9 +2,11 @@
 
 use App\Controllers\Backup;
 
+// Load Config file
+$config = require_once __DIR__.'/../config.php';
 
-$config = include __DIR__.'/../config.php';
-// Use full hosting path
-include $config->root_dir."vendor/autoload.php";
+// Composer Autoload
+require_once $config->root_dir."vendor/autoload.php";
 
+// Initialise App
 new Backup($config);
